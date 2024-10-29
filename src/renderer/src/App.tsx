34130d9a1ee04ac0a9df1fa952e675a4ec100f70
Yu, Xue | Xue | CLSD <xue.a.yu@rakuten.com>
2024-10-29 16:42:09 +0900

    add env.d.ts file

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 const { ipcRenderer } = window.require('electron')
 import './App.css';
 import TodoItem from './components/TodoItem';
